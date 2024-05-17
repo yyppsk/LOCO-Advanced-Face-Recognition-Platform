ad753490d2f23923a3717f97a6bffb6517fe62ab
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-05-18 03:21:42 +0530

    Improved Search Details

--- routes/routes.js
+++ routes/routes.js
@@ -55,6 +55,32 @@ router.get("/fetchUserInfobyid/:id", async (req, res) => {
   }
 });
 
+//FETCH AGENCY
+
+router.get("/fetchReportedBy/:id", async (req, res) => {
+  const reportedById = req.params.id;
+
+  try {
+    const client = await pool.connect();
+    const queryText = `
+      SELECT username, email
+      FROM appuser
+      WHERE user_id = $1;
+    `;
+    const result = await client.query(queryText, [reportedById]);
+    client.release();
+
+    if (result.rows.length === 0) {
+      res.status(404).json({ error: "Reported agency not found" });
+    } else {
+      res.status(200).json(result.rows[0]);
+    }
+  } catch (error) {
+    console.error("Error fetching reported agency information:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 //Dashboard Route
 
 router.get("/dashboardfetch", async (req, res) => {
